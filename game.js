119ca722fcb5bfae4a21017bef02079c1e26f247
Zi Tan <tanzitian3@gmail.com>
2024-07-17 10:52:44 -0400

    game file pt 2

--- game.js
+++ game.js
@@ -120,3 +120,97 @@ function fightDragon() {
   goFight();
 }
 
+function attack() {
+  text.innerText = "The " + enemys[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentItem].name + ".";
+  health -= getMonsterAttackValue(enemys[fighting].level);
+  if (isMonsterHit()) {
+    monsterHealth -= weapons[currentItem].power + Math.floor(Math.random() * xp) + 1;
+  } else {
+    text.innerText += " You miss.";
+  }
+
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    defeatMonster();
+  }
+  if (fighting === 2) {
+    winGame();
+  } else {
+    defeatMonster();
+  }
+  if (Math.random() <= .1 && inventory.length !== 1) {
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentItem--;
+  }
+}
+
+function getMonsterAttackValue(level) {
+  const hit = (level * 5) - (Math.floor(Math.random() * xp));
+  return hit > 0 ? hit : 0;
+}
+
+function isMonsterHit() {
+  return Math.random() > .2 || health < 20;
+}
+
+function dodge() {
+  text.innerText = "You dodge the attack from the " + enemys[fighting].name;
+}
+
+function defeatMonster() {
+  gold += Math.floor(enemys[fighting].level * 6.7);
+  xp += enemys[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
+
+function lose() {
+  update(locations[5]);
+}
+
+function winGame() {
+  update(locations[6]);
+}
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentItem = 0;
+  inventory = ["stick"];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
+}
+
+function pick(guess) {
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+  if (numbers.indexOf(guess) !== -1) {
+    text.innerText += "Right! You win 20 gold.";
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += "Wrong! You lose 10 health.";
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
+  }
+}
+
+document.getElementById('button4').onclick = viewStats;
+
