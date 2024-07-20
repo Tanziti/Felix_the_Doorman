a2ec928c8646f232691f92daf678284f2da94f4e
Zi Tan <tanzitian3@gmail.com>
2024-07-20 18:13:38 -0400

    data file

---
+++ data.js
@@ -0,0 +1,25 @@
+// data.js
+export const peoples = [
+    { name: "Magic Salesman", type: "salesman" },
+    { name: "Johnson", type: "resident" },
+    { name: "Alice", type: "resident" },
+    { name: "Clint", type: "postman" },
+    { name: "Al", type: "superintendent" },
+    { name: "Ben", type: "elevator repairman" },
+    { name: "Chris", type: "postman" }
+  ];
+  
+  export const weapons = [
+    { name: "phone", power: 5 },
+    { name: "keys", power: 30 },
+    { name: "bat", power: 50 },
+    { name: "sword", power: 100 }
+  ];
+  
+  export const enemys = [
+    { name: "slime", level: 2, health: 15 },
+    { name: "fanged beast", level: 8, health: 60 },
+    { name: "dragon", level: 20, health: 300 }
+  ];
+  
+ 
