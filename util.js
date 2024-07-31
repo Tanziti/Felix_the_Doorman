4aa7d8f59a78e81b5e4080ef5bab12821a71c6ba
Zi Tan <tanzitian3@gmail.com>
2024-07-30 22:38:46 -0400

    test

---
+++ util.js
@@ -0,0 +1,34 @@
+function hideButton(button) {
+    button.style.display = 'none';
+  }
+  
+  function showButton(button) {
+    button.style.display = 'block';
+  }
+  
+  function update(location) {
+    console.log("Updating location:", location); // Log the location data to check if it is correct
+  
+    monsterStats.style.display = "none";
+  
+    const buttons = [button1, button2, button3, button4];
+  
+    buttons.forEach((button, index) => {
+        if (index < location['button text'].length) {
+            button.innerText = location['button text'][index];
+            button.onclick = location['button functions'][index];
+  
+            if (location['button visibility'][index]) {
+                showButton(button);
+            } else {
+                hideButton(button);
+            }
+        } else {
+            hideButton(button);
+        }
+    });
+  
+    text.innerHTML = location.text;
+    console.log("Updated text content:", text.innerHTML); // Log the updated text content
+  }
+  
--- utils.js
+++ utils.js
@@ -1,33 +1,31 @@
-function hideButton(button) {
-  button.style.display = 'none';
-}
+// utils.js
 
-function showButton(button) {
-  button.style.display = 'block';
-}
-
-function update(location) {
-  console.log("Updating location:", location); // Log the location data to check if it is correct
-
-  monsterStats.style.display = "none";
-
-  const buttons = [button1, button2, button3, button4];
-
-  buttons.forEach((button, index) => {
-      if (index < location['button text'].length) {
-          button.innerText = location['button text'][index];
-          button.onclick = location['button functions'][index];
-
-          if (location['button visibility'][index]) {
-              showButton(button);
-          } else {
-              hideButton(button);
-          }
-      } else {
-          hideButton(button);
-      }
-  });
-
-  text.innerHTML = location.text;
-  console.log("Updated text content:", text.innerHTML); // Log the updated text content
-}
+export function update(location) {
+    monsterStats.style.display = "none";
+    button1.innerText = location['button text'][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button4.innerText = location["button text"][3];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    button4.onclick = location["button functions"][3];
+    text.innerText = location.text;
+  }
+  
+  export function getMonsterAttackValue(level) {
+    const hit = (level * 5) - (Math.floor(Math.random() * xp));
+    return hit > 0 ? hit : 0;
+  }
+  
+  export function isMonsterHit() {
+    return Math.random() > .2 || health < 20;
+  }
+  
+  export function hideButton(button) {
+    button.style.display = 'none';
+  }
+  
+  export function showButton(button) {
+    button.style.display = 'block';
+  }
